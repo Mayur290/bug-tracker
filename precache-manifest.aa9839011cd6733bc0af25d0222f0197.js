b7d1baa0d48be7494b82c43ad53a863566bcf254
Mayur290 <mayurbansal98@gmail.com>
2020-10-14 09:56:19 +0530

    Updates

---
+++ precache-manifest.aa9839011cd6733bc0af25d0222f0197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b10445569236892971270d9d90628d",
+    "url": "/bug-tracker/index.html"
+  },
+  {
+    "revision": "1f1ca30d2fd665302cb7",
+    "url": "/bug-tracker/static/css/main.bd8da468.chunk.css"
+  },
+  {
+    "revision": "3d0b8a70702e8b33b346",
+    "url": "/bug-tracker/static/js/2.aee681d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bug-tracker/static/js/2.aee681d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1ca30d2fd665302cb7",
+    "url": "/bug-tracker/static/js/main.f37f274c.chunk.js"
+  },
+  {
+    "revision": "0c6586b9178d98f6f7dd",
+    "url": "/bug-tracker/static/js/runtime-main.91cd1057.js"
+  }
+]);
